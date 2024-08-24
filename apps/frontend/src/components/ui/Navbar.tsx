5aed0123578f72fb3e256d4689725fa962c9295a
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-08-24 22:13:23 +0530

    Sevice Page Route

--- apps/frontend/src/components/ui/Navbar.tsx
+++ apps/frontend/src/components/ui/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar: React.FC = () => {
                 <Link to="/feeds" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Community Updates</Link>
               </li>
               <li>
-                <Link to="/services" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
+                <Link to="/services " className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
               </li>
               <li>
                 <Link to="/offer-support" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Offer Support</Link>
